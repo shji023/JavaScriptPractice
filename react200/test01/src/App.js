7567eafb9a02a444ff589fc615fb2cc6356fddf5
jiyoung <shyunji023@gmail.com>
2022-03-25 17:05:34 +0900

    React기초

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
-// Bpp.js파일에서 export default로 되어있는 컴포넌트를 가지고 오시오
-// 가져온 컴포넌트 이름을 여기서는 Bpp라는 이름으로 사용할 것이다.
-import Bpp,{Cpp} from './Bpp';
+
 // 함수형 컴포넌트 rsf
 function App() {
   return (
     <div>
+      <Bpp/>
       <h1>호랑이</h1>
-      <Bpp></Bpp>
-      <Cpp></Cpp>
+      
     </div>
   );
+}
+
+class Bpp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>독수리</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
