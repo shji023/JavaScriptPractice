f22254220d3f622c0ee59459d4a91e0349b7d89d
jiyoung <shyunji023@gmail.com>
2022-03-28 16:22:31 +0900

    12장

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -2,51 +2,77 @@ import React,{Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  // 생성자 함수를 이용하는 방법
-  f1(){
-    let F1 = (function(){
-      // 생성자 함수
-      function F1(num){
-        this.num = num;
-        F1.prototype.showNum = function(){
-          console.log(this.num);
-        }
-      }
-      return F1;
+  // 멤버함수
+  f1() {console.log(1);}
+  f2 = function (){
+    console.log(2);
+  }
+  f3 = () =>{
+
+  }
+  render() {
+    this.f1();
+    this.f2();
+    this.f3();
+
+    // 내부 함수
+    // 함수 안에서 함수를 작성할 수 있다.
+    function f4(){
+      console.log(4);
+    }
+    let f5 = function(){
+      console.log(5);
+    }
+    let f6 = ()=>{
+      console.log(6);
+    }
+    f4();
+    f5();
+    f6();
+
+    // 즉시실행함수
+    (function(){
+      console.log(7);
     })();
-    let obj = new F1(100);
-    console.log(obj.num);
 
-    obj.showNum();
-  }
-  // 클래스를 이용한 방법
-  f2(){
-   class F1{
-     constructor(num){
-       this.num = num;
-     }
-     showNum(){
-       console.log(this.num);
-     }
-   }
-   let obj = new F1(100);
-   console.log(obj.num);
-   obj.showNum();
-  }
-  render(){
-    return(
+    // 객체 함수
+    let obj = {
+      f9:function(){console.log(9)},
+      f10:()=>{console.log(10)},
+      f11(){console.log(11)},
+    }
+
+    obj.f9();
+    obj.f10();
+    obj.f11();
+
+    // 생성자 함수
+    function Person(){
+      this.f12 = function(){
+        console.log(12);
+      }
+      this.f13 = ()=>{
+        console.log(13);
+      }
+      Person.prototype.f14 = function(){console.log(14)};
+      Person.prototype.f15 = ()=>{console.log(15)};
+    }
+    let p = new Person();
+    p.f12(); p.f13(); p.f14(); p.f15();
+
+    class Fruit {
+      f16(){};
+      f17 = function(){console.log(17);};
+      f18 = ()=>{console.log(18);};
+    }
+    let f = new Fruit();
+    f.f16(); f.f17(); f.f18();
+    return (
       <div>
-        <h1>App</h1>
-        <button onClick={()=>{
-          this.f1();
-        }}>버튼1</button>
-        <button onClick={()=>{
-          this.f2();
-        }}>버튼2</button>
+        
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
