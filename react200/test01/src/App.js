71b2d96ba6ba6d0106b9930c50360d80d8b7f3e0
jiyoung <shyunji023@gmail.com>
2022-03-29 16:02:24 +0900

    jquery

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,30 +1,18 @@
 import React,{Component} from 'react';
-import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
-  f1(){
-    let ar = [1,2,3,4];
-    let br = ar.map( x => x * 2);
-    console.log(br);
-  }
-  f2(){
-    let ar = [
-      {name:'tiger', age:10},
-      {name:'lion', age:20},
-    ];
-    let br = ar.map((v,i)=>{
-      let obj = {};
-      // obj['tiger'];
-      obj[v.name] = i * v.age + v.age;
-      return obj;
-    });
-    console.log(br);
+  f1(e){
+    console.log('call');
+    let inputText = $('#inputId').val();
+    console.log(inputText);
   }
   render() {
     return (
       <div>
-        <button onClick={()=>{this.f1()}}>버튼</button>
-        <button onClick={()=>{this.f2()}}>버튼</button>
+        <input id = 'inputId' name='inputName' />
+        <button onClick={(e)=>{}}>버튼</button>
+        <button onClick={(e)=>{this.f1()}}>버튼</button>
       </div>
     );
   }
