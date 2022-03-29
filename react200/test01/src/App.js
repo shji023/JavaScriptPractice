7d9044548fb9eb914435b89f185af12d1dba0042
jiyoung <shyunji023@gmail.com>
2022-03-29 15:01:44 +0900

    setTimeout

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -3,33 +3,40 @@ import './App.css';
 
 class App extends Component {
   num = 10;
-  f1(){console.log(this.num)};
-  f2 = function(){console.log(this.num);};
-  f3 = ()=>{console.log(this.num);}
-  f4(){ // 여기서는 this를 알고있다
-    // function은 왜 this를 모를까
-    // 바인딩 방법1: 바인딩을 시킨다.
-    let temp = this; // temp는 함수 안에서 유효하기에 function에서 쓰면됨
-    (function(){ // 여기서는 this를 모른다
-      // 멤버 num을 제어할 수 있는가? - No. 함수안에서 만들어진 함수는 외부 멤버를 모른다
-      // console.log(this.num); error
-      console.log(temp.num);
-    })();
-    // 바인딩 방법2: 
-    (function(){ 
-      console.log(this.num);
-    }.bind(this))();
-    // 바인딩 방법3:
-    
-  }
-  render() {
-    this.f1();
+  f1(){
+    f2();
+    function f2(){
+      console.log(1);
+    }
     this.f2();
     this.f3();
     this.f4();
+    this.f5();
+  }
+
+  f2() {
+    console.log(2);
+  }
+  f3(){
+    let temp = this;
+    setTimeout(function(){
+      console.log(temp.num+1);
+    },1000);
+  }
+  f4(){
+    setTimeout(function(){
+      console.log(this.num+2);
+    }.bind(this),2000);
+  }
+  f5(){
+    setTimeout(()=>{
+      console.log(this.num+3);
+    },3000);
+  }
+  render() {
     return (
       <div>
-        
+        <button onClick={()=>{this.f1()}}>버튼</button>
       </div>
     );
   }
