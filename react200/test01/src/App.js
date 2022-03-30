62115118d7baa92ee8443b09aadd281c9614090f
jiyoung <shyunji023@gmail.com>
2022-03-30 15:18:10 +0900

    일반 컴포넌트

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,58 +1,23 @@
 import React,{Component,useEffect,useState} from 'react';
-// 함수 호출에 인수전달이 없다는 전제하에서는 약식 표기법을 사용할 수 있다.
+
+//                    setState(Render call)
+// Component 동일 값            o
+// Component 다른 값            o
+// PureComponent 동일 값        x
+// PureComponent 다른 값        o
 class App extends Component {
-  count = 999;
   constructor(props){
     super(props);
-    this.state={num:10,};
+    this.state = {x:10};
   }
-  // 정석 코드: 오리지널 코드
-  f1 = ()=> {
-    console.log('');
-    this.setState({num:this.state.num+1});
-  }
-  // setState를 사용하지 않으면 render()호출이 안된다.
-  f2 = ()=> {
-    console.log('');
-    this.state.num +=1;
-    this.count++;
-    // this.state.num += 1; // render함수를 호출하지 않기 때문에 적용x
-  }
-  // forceUpdate함수 이용하여 강제로 render호출
-  f3 = ()=> {
-    console.log('');
-    // 사실 이 목적은 아니고 
-    this.state.num += 1; // render함수를 호출하지 않기 때문에 적용x
-    // 이 목적을 위하여 forceUpdate()함수가 사용되어 질 수 있다.
-    this.count++;
-    this.forceUpdate();
-  }
-  f4 = ()=>{
-    this.state.num +=1;
-    this.render();
-  }
-  // shouldComponentUpdate 거치고 들어 
-  f5=()=>{
-    this.setState({num:this.state.num +10});
-  }
-  shouldComponentUpdate(){
-    console.log('shouldComponentUpdate');
-    // return이 false이면 render() 호출하지 않음
-    // counting에 변화가 일어나지 않음
-    return false;
+  f1=()=>{
+    this.setState({x:this.state.x});
   }
   render() {
-    console.log(this.state.num);
+    console.log('render');
     return (
       <div>
-        <h1>App</h1>
         <button onClick={this.f1}>버튼1</button>
-        <button onClick={this.f2}>버튼2</button>
-        <button onClick={this.f3}>버튼3</button>
-        <button onClick={this.f4}>버튼4</button>
-        <button onClick={this.f5}>버튼5</button>
-        <h3>{this.state.num}</h3>
-        <h3>{this.count}</h3>
       </div>
     );
   }
