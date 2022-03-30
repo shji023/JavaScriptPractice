e5f29b2992e29adc2426b7e67bf18211b0a4c82e
jiyoung <shyunji023@gmail.com>
2022-03-30 09:32:43 +0900

    true false props

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,41 +1,35 @@
 import React,{Component} from 'react';
-import PropsTypes from 'prop-types';
+
+// 1. 값을 전달하지 않는 경우 boolean으로 설정된다.
+// 2. 디폴트 값은 true이다.
+// 3. 받은 값은 삼항연산을  사용할 수 있다.
+// 4. true/false 자체를 출력할때는 toString()을 사용
 class App extends Component {
   render() {
     return (
       <div>
         <h1>App</h1>
-        <Bpp a={10} b={'tiger'} c={true} d ={[0,1,2]} e={{x:10, y:20}} f={()=>{return 99;}}/>
+        <Bpp a b={false} c={true}/>
       </div>
-      
     );
   }
 }
 
 class Bpp extends Component{
   render(){
-    let {a, b, c, d, e, f} = this.props;
+    let {a, b, c} = this.props;
     return (
       <div>
-        <h1>{a}</h1>
-        <h1>{b}</h1>
+        <h1>{a.toString()}</h1>
+        <h1>{b?'호랑이':'코끼리'}</h1>
+        <h1>{c?'호랑이':'코끼리'}</h1>
         <h1>{c}</h1>
-        <h1>{d}</h1>
-        <h1>{e.x}{e.y}</h1>
-        <h1>{f()}</h1>
       </div>
      
     )
   }
 }
-Bpp.propTypes = {
-  a:PropsTypes.number,
-  b:PropsTypes.string,
-  c:PropsTypes.bool,
-  d:PropsTypes.array,
-  e:PropsTypes.object,
-  f:PropsTypes.func,
-}
+
 
 export default App;
 
