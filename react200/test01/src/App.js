6a79faa29051ee7a580387ed2152d99c3d326545
jiyoung <shyunji023@gmail.com>
2022-03-30 09:21:09 +0900

    다양한 프롭스

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     return (
       <div>
         <h1>App</h1>
-        <Bpp tiger={100} lion={100}/>
+        <Bpp a={10} b={'tiger'} c={true} d ={[0,1,2]} e={{x:10, y:20}} f={()=>{return 99;}}/>
       </div>
       
     );
@@ -14,28 +14,29 @@ class App extends Component {
 
 class Bpp extends Component{
   render(){
-    let obj = {a:1, b:2};
-    let {a, b} = obj;
-    console.log(a,b);
-    console.log(this.props);
-
-    let {tiger,lion,...others} = this.props;
+    let {a, b, c, d, e, f} = this.props;
     return (
       <div>
-        <h1>{this.props.tiger}</h1>
-        <h1>{this.props.lion}</h1>
-        <h1>{tiger}</h1>
-        <h1>{lion}</h1>
+        <h1>{a}</h1>
+        <h1>{b}</h1>
+        <h1>{c}</h1>
+        <h1>{d}</h1>
+        <h1>{e.x}{e.y}</h1>
+        <h1>{f()}</h1>
       </div>
+     
     )
   }
 }
-// tiger는 string 타입이어야 한다는 것을 강제적으로 설정
-// 안지키면 경고뜸
 Bpp.propTypes = {
-  tiger:PropsTypes.string,
-  lion:PropsTypes.string,
-};
+  a:PropsTypes.number,
+  b:PropsTypes.string,
+  c:PropsTypes.bool,
+  d:PropsTypes.array,
+  e:PropsTypes.object,
+  f:PropsTypes.func,
+}
+
 export default App;
 
 
