3ff6e20e94259cc0a91ac4d7c30d39e7836fff6d
jiyoung <shyunji023@gmail.com>
2022-03-30 15:41:04 +0900

    shallowEqualObjects

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,29 +1,28 @@
 import React,{Component,useEffect,useState,PureComponent} from 'react';
+import { shallowEqualObjects } from 'shallow-equal';
 
-//                    setState(Render call)
-// Component 동일 값            o
-// Component 다른 값            o
-// PureComponent 동일 값        x
-// PureComponent 다른 값        o
-// state 값이 갱신이 될 때만 render을 호출하겠다 => pure
-class App extends PureComponent {
-//class App extends Component {
+class App extends Component {
   constructor(props){
     super(props);
-    this.state = {x:10};
+    this.state = {x:0};
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log(this.state,nextState);
+    console.log(!shallowEqualObjects(this.state, nextState));
+    return !shallowEqualObjects(this.state, nextState);
   }
   f1=()=>{
     this.setState({x:this.state.x});
   }
   f2=()=>{
-    this.setState({x:this.state.x+1});
+    this.setState({x:this.state.x+10});
   }
   render() {
     console.log('render');
     return (
       <div>
-        <button onClick={this.f1}>버튼1</button>
-        <button onClick={this.f2}>버튼2</button>
+        <button onClick={()=>{this.f1()}}>버튼1</button>
+        <button onClick={()=>{this.f2()}}>버튼2</button>
       </div>
     );
   }
