2ec01e92806f894849205b36834d11efc6c2993b
jiyoung <shyunji023@gmail.com>
2022-03-31 09:37:18 +0900

    useState

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,41 +1,41 @@
 import React,{Component,useEffect,useState,PureComponent} from 'react';
-import { shallowEqualArrays, shallowEqualObjects } from 'shallow-equal';
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {x:0};
+// 6. 전역변수 선언하고 함수에서 사용가능
+let value;
+function sampleUseState(num){
+  if(value===undefined){
+    value = num;
   }
-  shouldComponentUpdate(nextProps, nextState){
-    console.log(this.state,nextState);
-    console.log(!shallowEqualObjects(this.state, nextState));
-    return !shallowEqualObjects(this.state, nextState);
-  }
-  f1=()=>{
-    this.setState({x:this.state.x});
-  }
-  f2=()=>{
-    this.setState({x:this.state.x+10});
-  }
-  f3=()=>{
-    let ar1 = [1, 2, 3];
-    let ar2 = [1, 2, 3];
-    let ar3 = [1, 2, 4];
-    console.log(shallowEqualArrays(ar1, ar2)); // true
-    console.log(shallowEqualArrays(ar1, ar3)); // false
-  }
-  render() {
-    console.log('render');
-    return (
-      <div>
-        <button onClick={()=>{this.f1()}}>버튼1</button>
-        <button onClick={()=>{this.f2()}}>버튼2</button>
-        <button onClick={()=>{this.f3()}}>버튼3</button>
-      </div>
-    );
-  }
+  return [value, (v)=>{value = v;}];
 }
+let count = 1000;
+function App(props) {
+  count++;
 
+  // 5. 모호한 코드로 보일 수 있다.
+  // 지역변수처럼 보이는데 이렇게 전역변수처럼 행동하는가?
+  // const면 값을 변경할 수 없는데?
+  const [value, setValue] = useState(10);
+
+  // 7. 비구조화 할당 문법을 테스트
+  let num2 = 10;
+  let ar = [num2, ()=>{console.log(9999);}];
+  let [a, b] = ar;
+  console.log('ar:', a, b);
+
+  // 1. 비동기 함수 이다.
+  // 2. render 이후 호출(componentDidMount하고 동일한 기능)
+  // setValue(11); >> 계속 rerendering되기에 무한루프 빠져버림
+  useEffect(()=>{
+    console.log(3);
+  });
+  console.log(2);
+  return (
+    <div>
+      setvalue가 rerendering을 시킴
+      <button onClick={()=>{setValue(value+1)}}>버튼</button>
+    </div>
+  );
+}
 export default App;
 
 
