56c264fcffbd977b5a89a91c9b122434c68e399f
jiyoung <shyunji023@gmail.com>
2022-04-01 14:46:46 +0900

    인자 넘겨주기

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -2,22 +2,27 @@ import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-  f1 = (a, b) => {
-    new Promise ((resolve,reject)=>{
-      a>b?resolve(10):reject(20)
-    })
-  .then((num)=>{
-    console.log(num);
-  })  
-  .catch((num)=>{
-    console.log(num);
-  });
+  f1 = ()=>{
+    console.log(1);
+  }
+  f2 = ()=>{
+    console.log(2);
+  }
+  f3 = arg=>{
+    console.log(3, arg);
+  }
+  f4 = arg=>{
+    console.log(4, arg);
   }
   render() {
     return (
       <div>
-        <button onClick={()=>{this.f1(4, 3)}}>버튼1</button>
-        <button onClick={()=>{this.f1(3, 4)}}>버튼1</button>
+        <button onClick={()=>{this.f1()}}>버튼1</button>
+        <button onClick={this.f2}>버튼2</button>
+        <button onClick={()=>{this.f3(100)}}>버튼3</button>
+        <button onClick={(e)=>{this.f4(e)}}>버튼4</button>
+
+        <div onClick={this.f1}></div>
       </div>
     );
   }
