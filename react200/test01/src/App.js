fc35ed830ecf06d23394fbbf33406d7fdf7e23f3
jiyoung <shyunji023@gmail.com>
2022-04-01 15:13:01 +0900

    pageX,Y

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -2,24 +2,28 @@ import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-  f1 = (e)=>{
-    let inputData = e.target.value;
-    console.log(inputData);
+  constructor(props){
+    super(props);
+    this.state={x:0, y:0};
   }
-  f2 = (e)=>{
-    console.log(e.target.value);
+  f1 = (e)=>{
+    console.log(e.pageX, e.pageY);
+    this.setState({x:e.pageX, y:e.pageY});
   }
   
   render() {
     return (
+      
       <div>
-        <input type='text' onChange={this.f1}></input>
+        <div style={{background:'red'}} onMouseMove={(e)=>{this.f1(e)}}>호랑이</div>
+        <h6>{this.state.x}{this.state.y}</h6>
+        {/* <input type='text' onChange={this.f1}></input>
         <select onChange={this.f2}>
           <option value='호랑이1'>호랑이5</option>
           <option value='호랑이2'>호랑이6</option>
           <option value='호랑이3'>호랑이7</option>
           <option value='호랑이4'>호랑이8</option>
-        </select>
+        </select> */}
       </div>
     );
   }
