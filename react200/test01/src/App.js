510eda456fc88319da87fa91a66abf0a40ae2cf8
jiyoung <shyunji023@gmail.com>
2022-04-01 14:38:10 +0900

    resolve reject

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -2,50 +2,25 @@ import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-  f1 = () => {
-    new Promise ((resolve)=>{
-      console.log(1);
-      resolve();
+  f1 = (a, b) => {
+    new Promise ((resolve,reject)=>{
+      a>b?resolve(10):reject(20)
     })
-    .then(()=>{
-      console.log(2);
-    })
+  .then((num)=>{
+    console.log(num);
+  })  
+  .catch((num)=>{
+    console.log(num);
+  });
   }
-  f2 = () => {
-    new Promise ((resolve)=>{
-      console.log(1);
-      resolve(1000);
-    })
-    .then((result)=>{
-      console.log(2,result);
-    })
-  }
-  f3 = () => {
-    new Promise ((resolve,)=>{
-      console.log(1);
-      resolve(1000);
-    })
-    .then((result)=>{
-      console.log(2,result);
-      return 2000;
-    })
-    .then((result)=>{
-      console.log(3,result);
-    })
-    .then(()=>{
-      console.log(4);
-    })
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.f1}>버튼1</button>
-        <button onClick={this.f2}>버튼2</button>
-        <button onClick={this.f3}>버튼3</button>
+        <button onClick={()=>{this.f1(4, 3)}}>버튼1</button>
+        <button onClick={()=>{this.f1(3, 4)}}>버튼1</button>
       </div>
     );
   }
 }
-
+  
 export default App;
