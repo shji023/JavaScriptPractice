ab1d2bbd87e2960abd0615ac4052cdc4ff02fc8a
jiyoung <shyunji023@gmail.com>
2022-04-01 14:29:28 +0900

    fromise resolve

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -3,19 +3,46 @@ import React, { Component } from 'react';
 
 class App extends Component {
   f1 = () => {
-    console.log(1);
-    this.f2(1000, ()=>{
-      console.log(3);
-    });
+    new Promise ((resolve)=>{
+      console.log(1);
+      resolve();
+    })
+    .then(()=>{
+      console.log(2);
+    })
   }
-  f2 = (p, c) => {
-    console.log(2, p);
-    c();
+  f2 = () => {
+    new Promise ((resolve)=>{
+      console.log(1);
+      resolve(1000);
+    })
+    .then((result)=>{
+      console.log(2,result);
+    })
   }
+  f3 = () => {
+    new Promise ((resolve,)=>{
+      console.log(1);
+      resolve(1000);
+    })
+    .then((result)=>{
+      console.log(2,result);
+      return 2000;
+    })
+    .then((result)=>{
+      console.log(3,result);
+    })
+    .then(()=>{
+      console.log(4);
+    })
+  }
+
   render() {
     return (
       <div>
         <button onClick={this.f1}>버튼1</button>
+        <button onClick={this.f2}>버튼2</button>
+        <button onClick={this.f3}>버튼3</button>
       </div>
     );
   }
