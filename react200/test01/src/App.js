85a385b5e3e7a7806b76a3e9138c86eeb33e5c21
jiyoung <shyunji023@gmail.com>
2022-04-01 10:47:14 +0900

    axios get post

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,59 +1,35 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-  f1 = async() => {
-    await new Promise((resolve)=>{
-      setTimeout(()=>{
-        console.log('1'); // 3초 뒤 출력
-        resolve(); // then으로 이동 시켜 then이 출력됨
-      },3000);
-    });
-    await new Promise((resolve)=>{
-      setTimeout(()=>{
-        console.log('2'); // 2초 뒤 출력
-        resolve(); // then으로 이동 시켜 then이 출력됨
-      },2000);
-    })
-    await new Promise((resolve)=>{
-      setTimeout(()=>{
-        console.log('3'); // 1초 뒤 출력
-        resolve(); // then으로 이동 시켜 then이 출력됨
-      },1000);
-    })
-    console.log('test'); // test먼저 출력 >> 비동기
+  // get
+  f1 = async () => {
+    //fetch('http://date.jsontest.com');
+    // axios.get('http://date.jsontest.com')
+    // .then((response)=>{
+    //   console.log(response);
+    //   console.log(response.data);
+    //   console.log(response.data.date);
+    // })
+    const data = await axios.get('http://date.jsontest.com')
+    console.log(data);
+    
   }
-
-  myFetch = (num, ms) => {
-    new Promise((resolve)=>{
-      setTimeout(()=>{
-        console.log(num); 
-      },ms);
-    });
-  }
-  f6 = async()=>{
-    await this.myFetch(1, 3000);
-    await this.myFetch(2, 2000);
-    await this.myFetch(3, 1000);
-  }
-  f7 = async()=>{
-    const response = await fetch(
-      'http://date.jsontest.com',
-      {
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body:{a:'react', b:200}
-      }
-    );
-    const body = await response.json();
-    console.log(body.date);
+  // post
+  f2 = async () => {
+    const data = await axios.post('http://date.jsontest.com',{a:10, b:20})
+    // axios.post('http://date.jsontest.com',{a:10, b:20})
+    // .then((response)=>{
+    //  console.log(1, response.data.date);
+    // })
+    console.log(data);
   }
   render() {
     return (
       <div>
         <h2>호랑이</h2>
-        <button onClick={this.f7}>버튼1</button>
+        <button onClick={this.f1}>버튼1</button>
+        <button onClick={this.f2}>버튼2</button>
       </div>
     );
   }
