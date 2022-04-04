8c69a1baa153d3ded6a6ee08ad4f6ec6d3499e29
jiyoung <shyunji023@gmail.com>
2022-04-04 09:50:52 +0900

    Provider Consumer2

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -3,10 +3,27 @@ import React, { Component } from 'react';
 const {Provider, Consumer} = React.createContext();
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={num:0};
+  }
+  f1=(value)=>{
+    this.setState({num:this.state.num+value});
+  }
   render() {
+    const obj={a:10, b:20};
+    const obj2 = {...obj,f1:()=>{}};
+    console.log(obj2);
+    console.log(this.state);
+
+    const tiger={
+      ...this.state,f1:this.f1
+    }
+    console.log(tiger);
     return (
       <div>
-        <Provider value='호랑이'>
+        <h3>{this.state.num}</h3>
+        <Provider value={tiger}>
           <Bpp />
         </Provider>
       </div>
@@ -28,16 +45,19 @@ class Cpp extends Component {
   render() {
     return (
       <div>
+        <h3>Cpp</h3>
         <Consumer>
           {
-            //cv => <h3>{cv}</h3>
-            cv => <h3>{`result:${cv}`}</h3>
+            cv=>
+            <button onClick={e=>{cv.f1(10)}}>버튼
+              {/* {cv.num} */}
+            </button>
           }
         </Consumer>
-        <h3>Cpp</h3>
       </div>
     );
   }
 }
+
 
 export default App;
