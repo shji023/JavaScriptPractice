f3e950b77aedd68f863b08caa3bdc2a67495e2be
jiyoung <shyunji023@gmail.com>
2022-04-04 11:52:46 +0900

    redux

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,63 +1,57 @@
 import React, { Component } from 'react';
-
-const {Provider, Consumer} = React.createContext();
+import { connect } from 'react-redux';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={num:0};
-  }
-  f1=(value)=>{
-    this.setState({num:this.state.num+value});
-  }
   render() {
-    const obj={a:10, b:20};
-    const obj2 = {...obj,f1:()=>{}};
-    console.log(obj2);
-    console.log(this.state);
+    return (
+      <div>
+        <h3>App</h3>
+        <button onClick={this.props.onInc}>버튼</button>
+        {/* // this.props.monkey */}
+        <h3>{this.props.num}</h3>
+      </div>
+    );
+  }
+}
+// 여기에서 전역변수를 정의
+// state = {num:10} 단 한번만 사용된다.
+export function reducers(state = {num: 10},action){
+  console.log('reducers',action.type, state.num);
+  return {num:state.num+1,};
+}
 
-    const tiger={
-      ...this.state,f1:this.f1
+const dispatchToProps = (dispatch)=>{
+  console.log('dispatchToProps');
+  return{
+    // onInc함수가 호출이 되면
+    // reducers함수에 'INC'라고 하는 문자열을 전송하세요
+    onInc: ()=>{
+      console.log('dispatch: onInc');
+      dispatch({type:'INC',});
     }
-    console.log(tiger);
-    return (
-      <div>
-        <h3>{this.state.num}</h3>
-        <Provider value={tiger}>
-          <Bpp />
-        </Provider>
-      </div>
-    );
-  }
+  };
 }
 
-class Bpp extends Component {
-  render() {
-    return (
-      <div>
-        <Cpp />
-      </div>
-    );
-  }
-}
-
-class Cpp extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Cpp</h3>
-        <Consumer>
-          {
-            cv=>
-            <button onClick={e=>{cv.f1(10)}}>버튼
-              {/* {cv.num} */}
-            </button>
-          }
-        </Consumer>
-      </div>
-    );
-  }
+const stateToProps = (state)=>{
+  console.log('stateToProps');
+  // num: app에서 사용될 key값
+  // num을 monkey라고 해도 상관없음
+  return {num: state.num};
 }
 
 
-export default App;
+export default App = connect(
+  stateToProps,
+  dispatchToProps
+)(App);
+
+// let connect = function(f1, f2){
+//   return function(){
+//     return class {};
+//   }
+// }
+
+// f1(
+//   stateToProps, 
+//   dispatchToProps
+// )(App);
--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, { reducers } from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-
+// const store = createStore( 리듀스함수 );
+// store => 전역변수를 저장할 공간
+const store = createStore(reducers);
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
