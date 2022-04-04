47cbd7d671c5c91d8743ed823ee8c3becd0c00f1
jiyoung <shyunji023@gmail.com>
2022-04-04 16:29:15 +0900

    store, reducer

--- test01/redux.js
+++ test01/redux.js
@@ -1,19 +1,13 @@
+import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+// reducer: 데이터를 modify하는 곳(함수)
+// 예를 들어 increase나 decrease함
+const reducer = ()=>{};
+// store: 데이터를 저장하는곳
+const store = createStore(reducer);
 
-let count = 0;
-number.innerText = count;
-const updateText = ()=>{
-    number.innerText = count;
-}
-const handleAdd=()=>{
-    count = count+1;
-    updateText();
-}
-const handleMinus=()=>{
-    count = count-1;
-    updateText();
-}
+
 add.addEventListener("click",handleAdd);
 minus.addEventListener("click",handleMinus);
