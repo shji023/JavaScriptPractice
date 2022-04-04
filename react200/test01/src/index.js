e11a70eacf25ee67e479bb0222ba874bab2fdd16
jiyoung <shyunji023@gmail.com>
2022-04-04 17:23:55 +0900

    refactoring

--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -2,17 +2,28 @@ import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
-// function that can modify your data
-const countModifier = (count = 0,action)=>{
-  //console.log(count, action);
-  if(action.type==="ADD"){
-    return count +1;
-  } else if (action.type === "MINUS"){
-    return count -1;
-  } else{
-    return 0;
-  }
 
+const ADD = "ADD";
+const MINUS = "MINUS"
+// function that can modify your data
+// reducer가 리턴하는 것은 무엇이든지 어플리케이션의 state가 되는것
+const countModifier = (count = 0,action)=>{
+  // //console.log(count, action);
+  // if(action.type==="ADD"){
+  //   return count +1;
+  // } else if (action.type === "MINUS"){
+  //   return count -1;
+  // } else{
+  //   return 0;
+  // }
+  switch(action.type){
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
@@ -33,11 +44,14 @@ countStore.subscribe(onChange);
 // countStore.dispatch({type:"ADD"});
 // countStore.dispatch({type:"MINUS"});
 // console.log(countStore.getState()); // 4
+
+// action은 object이어야함
+// 액션은 정의되지 않은 type property가 있으면 안된다.
 const handleAdd = ()=> {
-  countStore.dispatch({type:"ADD"});
+  countStore.dispatch({type:ADD});
 }
 const handleMinus = () => {
-  countStore.dispatch({type:"MINUS"});
+  countStore.dispatch({type:MINUS});
 }
 // anonymous function
 add.addEventListener("click",handleAdd)
