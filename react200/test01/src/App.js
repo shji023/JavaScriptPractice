b8989c1ee132928165ff3d4a39d31d68db636245
jiyoung <shyunji023@gmail.com>
2022-04-04 12:31:15 +0900

    refactoring 버튼+,-

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -6,31 +6,42 @@ class App extends Component {
     return (
       <div>
         <h3>App</h3>
-        <button onClick={this.props.onInc}>버튼</button>
+        <button onClick={this.props.onInc}>버튼+</button>
+        <button onClick={this.props.onDec}>버튼-</button>
         {/* // this.props.monkey */}
         <h3>{this.props.num}</h3>
       </div>
     );
   }
 }
+
+const INC = 'INC';
+const DEC = 'DEC';
+const inc = ()=>({type: INC,});
+const dec = ()=>({type: DEC,});
 // 여기에서 전역변수를 정의
 // state = {num:10} 단 한번만 사용된다.
 export function reducers(state = {num: 10},action){
   console.log('reducers',action.type, state.num);
-  return {num:state.num+1,};
+  switch(action.type){
+    case INC:
+      return {num:state.num+1,};
+    case DEC:
+      return {num:state.num-1,};
+    default: 
+    return state;
+  }
 }
+let f1 = ()=>{return 100;}
+let f2 = ()=>100;
+let f3 = ()=>{return {};}
+let f4 = ()=>({});
 
-const dispatchToProps = (dispatch)=>{
-  console.log('dispatchToProps');
-  return{
-    // onInc함수가 호출이 되면
-    // reducers함수에 'INC'라고 하는 문자열을 전송하세요
-    onInc: ()=>{
-      console.log('dispatch: onInc');
-      dispatch({type:'INC',});
-    }
-  };
-}
+const dispatchToProps = (dispatch)=>({
+  onInc: ()=>{dispatch(inc());},
+  onDec: ()=>{dispatch(dec());}
+});
+
 
 const stateToProps = (state)=>{
   console.log('stateToProps');
