a86620820176da820591a5e131da5d3fadfc8ea6
jiyoung <shyunji023@gmail.com>
2022-04-04 17:15:03 +0900

    number update, subscribe

--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -4,7 +4,7 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 // function that can modify your data
 const countModifier = (count = 0,action)=>{
-  console.log(count, action);
+  //console.log(count, action);
   if(action.type==="ADD"){
     return count +1;
   } else if (action.type === "MINUS"){
@@ -16,14 +16,30 @@ const countModifier = (count = 0,action)=>{
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
+
+// subscribe 는 우리에게 store안에 있는 변화들을 알 수 있게 해줌
+const onChange = ()=>{
+  number.innerText = countStore.getState();
+}
+// store변화가 있을 때마다 감지해서 불려질 것
+countStore.subscribe(onChange);
 
 // 어떻게 countModifier에게 action을 보낼 수 있지?
 // dispatch를 활용
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"MINUS"});
-console.log(countStore.getState()); // 4
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"MINUS"});
+// console.log(countStore.getState()); // 4
+const handleAdd = ()=> {
+  countStore.dispatch({type:"ADD"});
+}
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"});
+}
+// anonymous function
+add.addEventListener("click",handleAdd)
+minus.addEventListener("click",handleMinus)
+
