62de36ef2004ab47468896018449b888ca46da1e
jiyoung <shyunji023@gmail.com>
2022-04-05 10:24:32 +0900

    cpp

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <ConnectBpp/>
+        <ConnectCpp/>
       </div>
     );
   }
@@ -17,10 +18,21 @@ class Bpp extends Component {
   render() {
     return (
       <div>
-        <h1>BPp</h1>
+        <h1>Bpp</h1>
         <h1>{this.props.num}</h1>
         <button onClick={this.props.onInc}>증가</button>
         <button onClick={this.props.onDec}>감소</button>
+      </div>
+    );
+  }
+}
+
+class Cpp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Cpp</h1>
+        <h1>{this.props.num}</h1>
       </div>
     );
   }
@@ -39,10 +51,7 @@ const initState = {
 
 export const reducers = handleActions(
   {
-    [INC]:(state,action)=>{
-      console.log('Reducers: onInc');
-      return({num:state.num + 1})
-    },
+    [INC]:(state,action)=>({num:state.num + 1}),
     [DEC]:(state,action)=>({num:state.num - 1})
   },
   initState
@@ -50,20 +59,19 @@ export const reducers = handleActions(
 
 // hooks
 const ConnectBpp = ()=>{
-  const num = useSelector(state=>{
-    console.log('StateToProps');
-    return state.num
-  });
+  const num = useSelector(state=>(state.num));
   const dispatch = useDispatch();
   return(
     <Bpp num ={num}
-    onInc ={
-      ()=>{
-        console.log('Dispatch: onInc');
-        dispatch(inc());
-      }
-    }
+    onInc ={()=>dispatch(inc())}
     onDec ={()=>dispatch(dec())} />
+  )
+}
+
+const ConnectCpp = ()=>{
+  const num = useSelector(state=>state.num);
+  return (
+    <Cpp num = {num}/>
   )
 }
 export default App;
