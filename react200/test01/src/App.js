663e7763966a1c4dc7d3832735fd1f307a06cbe0
jiyoung <shyunji023@gmail.com>
2022-04-05 09:42:37 +0900

    DRS 순서 확인 코드

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -26,7 +26,7 @@ class Bpp extends Component {
   }
 }
 
-
+// reducers
 const INC = 'INC';
 const DEC = 'DEC';
 
@@ -39,43 +39,30 @@ const initState = {
 
 export const reducers = handleActions(
   {
-    [INC]:(state,action)=>({num:state.num + 1}),
+    [INC]:(state,action)=>{
+      console.log('Reducers: onInc');
+      return({num:state.num + 1})
+    },
     [DEC]:(state,action)=>({num:state.num - 1})
   },
   initState
 );
-  
-// const stateToProps = (state)=> ({num: state.num});
-// const dispatchToProps = dispatch => bindActionCreators({
-//   onInc: inc,
-//   onDec: dec,
-// }, dispatch)
-
-// bindActionCreators
-// const ConnectBpp = connect(
-//   state => ({num: state.num}),
-//   dispatch => bindActionCreators({
-//     onInc: inc,
-//     onDec: dec,
-//   }, dispatch)
-// )(Bpp);
-
-// 위에 코드랑 똑같음
-// const ConnectBpp = connect(
-//   state => ({num: state.num}),
-//   {
-//     onInc: inc,
-//     onDec: dec,
-//   }
-// )(Bpp);
 
 // hooks
 const ConnectBpp = ()=>{
-  const num = useSelector(state=>state.num);
+  const num = useSelector(state=>{
+    console.log('StateToProps');
+    return state.num
+  });
   const dispatch = useDispatch();
   return(
     <Bpp num ={num}
-    onInc ={()=>dispatch(inc())}
+    onInc ={
+      ()=>{
+        console.log('Dispatch: onInc');
+        dispatch(inc());
+      }
+    }
     onDec ={()=>dispatch(dec())} />
   )
 }
