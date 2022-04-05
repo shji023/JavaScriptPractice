5b71ea569f1d629c106843b239e123c572d318c6
jiyoung <shyunji023@gmail.com>
2022-04-05 09:30:07 +0900

    hooks로 수정

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,16 +1,82 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home  from './routes/Home';
-import Detail from './routes/Detail';
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Detail/>} />
-      </Routes>
-    </BrowserRouter>
-  )
+import React, { Component } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { createAction,handleActions } from 'redux-actions';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ConnectBpp/>
+      </div>
+    );
+  }
 }
 
+class Bpp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>BPp</h1>
+        <h1>{this.props.num}</h1>
+        <button onClick={this.props.onInc}>증가</button>
+        <button onClick={this.props.onDec}>감소</button>
+      </div>
+    );
+  }
+}
+
+
+const INC = 'INC';
+const DEC = 'DEC';
+
+const inc = createAction(INC);
+const dec = createAction(DEC);
+
+const initState = {
+  num:100
+}
+
+export const reducers = handleActions(
+  {
+    [INC]:(state,action)=>({num:state.num + 1}),
+    [DEC]:(state,action)=>({num:state.num - 1})
+  },
+  initState
+);
+  
+// const stateToProps = (state)=> ({num: state.num});
+// const dispatchToProps = dispatch => bindActionCreators({
+//   onInc: inc,
+//   onDec: dec,
+// }, dispatch)
+
+// bindActionCreators
+// const ConnectBpp = connect(
+//   state => ({num: state.num}),
+//   dispatch => bindActionCreators({
+//     onInc: inc,
+//     onDec: dec,
+//   }, dispatch)
+// )(Bpp);
+
+// 위에 코드랑 똑같음
+// const ConnectBpp = connect(
+//   state => ({num: state.num}),
+//   {
+//     onInc: inc,
+//     onDec: dec,
+//   }
+// )(Bpp);
+
+// hooks
+const ConnectBpp = ()=>{
+  const num = useSelector(state=>state.num);
+  const dispatch = useDispatch();
+  return(
+    <Bpp num ={num}
+    onInc ={()=>dispatch(inc())}
+    onDec ={()=>dispatch(dec())} />
+  )
+}
 export default App;
--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {Provider} from 'react-redux';
-import store from './store';
+import App, { reducers } from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+//const store = createStore(리듀서);
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store = {store}> 
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './App.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  //<React.StrictMode>
+    <App />,
+  //</React.StrictMode>,
+  document.getElementById('root')
+);
+*/
