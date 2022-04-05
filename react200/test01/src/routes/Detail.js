00102f0261a40c1888dc07b41d5c201f7d3c6c5c
jiyoung <shyunji023@gmail.com>
2022-04-05 19:48:16 +0900

    NomadRedux-TodoDetail

--- react200/test01/src/routes/Detail.js
+++ react200/test01/src/routes/Detail.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-function Detail(props) {
-    return (
-        <div>
-            detail
-        </div>
-    );
+function Detail({toDos}) {
+  const id = useParams().id;
+  const toDo = toDos.find((toDo)=>toDo.id===parseInt(id))
+  return (
+    <div>
+      {toDo?.text}
+      Created at: {toDo?.id}
+    </div>
+  );
+}
+function mapStateToProps(state){
+  // react-router-dom v6이상이라 안됨
+  // const {
+  //   match:{
+  //     params:{id}
+  //   }
+  // } = ownProps;
+  
+  // console.log(id);
+  return {toDos:state};
 }
 
-export default Detail;
+export default connect(mapStateToProps)(Detail);
+// export default Detail;
