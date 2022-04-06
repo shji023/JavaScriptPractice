e6bb6e4efa44201b26def45aea8884f4d63a62b8
jiyoung <shyunji023@gmail.com>
2022-04-06 14:37:26 +0900

    super market

--- react200/super-market/src/App.js
+++ react200/super-market/src/App.js
@@ -1,8 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react';
+import Counter from './components/Counter';
+import SuperMarket from './components/SuperMarket';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+        <hr />
+        <SuperMarket />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react200/super-market/src/components/SuperMarket.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SuperMarketTemplate from './SuperMarketTemplate';
+
+
+const SuperMarket = () => {
+  return <SuperMarketTemplate items={null} basket={null} />;
+};
+
+export default SuperMarket;
