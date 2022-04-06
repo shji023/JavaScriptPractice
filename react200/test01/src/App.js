3af2b172411b8c0bc0e5e089b2ad3a156c87dc48
jiyoung <shyunji023@gmail.com>
2022-04-06 10:16:06 +0900

    mobX

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,22 +1,46 @@
+import { action, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import React, { Component } from 'react';
-import BppContainer from './containers/BppContainer';
-import CppContainer from './containers/CppContainer';
-import DppContainer from './containers/DppContainer';
+
+class TigerStore {
+  count = 0;
+  constructor(){
+    makeObservable(this,{
+      count: observable,
+      increase: action,
+      decrease: action
+    });
+  }
+  increase(){this.count+=1;}
+  decrease(){this.count-=1;}
+}
+
+const tigerStore = new TigerStore();
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BppContainer/>
-        <CppContainer/>
-        <DppContainer/>
+        <h1>App</h1>
+        <button onClick={()=>{tigerStore.increase()}}>증가</button>
+        <button onClick={()=>{tigerStore.decrease()}}>감소</button>
+        <h1>{tigerStore.count}</h1>
+        <Bpp />
       </div>
     );
   }
 }
 
-export default App;
-
-// 컴포넌트: Bpp.js, Cpp.js >> components 폴더에서 관리
-// 리듀스: bpp.js, cpp.js >> modules, reduces 폴더에서 관리
-// 커넥트: BppContainer.js, CppContainer.js >> containers 폴더에서 관리
+class Bpp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bpp</h1>
+        <button onClick={()=>{tigerStore.increase()}}>증가</button>
+        <button onClick={()=>{tigerStore.decrease()}}>감소</button>
+        <h1>{tigerStore.count}</h1>
+      </div>
+    );
+  }
+}
+export default observer(App);
