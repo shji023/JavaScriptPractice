b759f13667b56bff98ef5b17e8774d9f81f4ba22
jiyoung <shyunji023@gmail.com>
2022-04-06 15:46:39 +0900

    counter와 연결

--- react200/super-market/src/components/SuperMarket.jsx
+++ react200/super-market/src/components/SuperMarket.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import SuperMarketTemplate from './SuperMarketTemplate';
-
+import ShopItemList from './ShopItemList';
+import BasketItemList from './BasketItemList';
 
 const SuperMarket = () => {
-  return <SuperMarketTemplate items={null} basket={null} />;
+  return (
+    <SuperMarketTemplate items={<ShopItemList />} basket={<BasketItemList />} />
+  );
 };
 
 export default SuperMarket;
