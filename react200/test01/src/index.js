4e0a7af0cbe8fc54776df46a634e84cc0023ea00
jiyoung <shyunji023@gmail.com>
2022-04-06 11:29:39 +0900

    불필요한 코드 삭제

--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -13,14 +13,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore } from 'redux';
-import { Provider } from 'react-redux';
 import rootReducer from './modules';
 
 const store = createStore(rootReducer);
 ReactDOM.render(
-  <Provider store = {store}> 
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
 
