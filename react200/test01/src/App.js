07022bdc1d1a456a57d9753b9a42e227a538eff5
jiyoung <shyunji023@gmail.com>
2022-04-06 12:30:36 +0900

    mobX TodoList

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,33 +1,95 @@
-import { makeAutoObservable } from 'mobx';
+import { action, autorun, computed, makeAutoObservable, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import React from 'react';
-import TimerView from './TimerView';
 
-class Timer {
-  secondsPassed = 0;
+class ObservableTodoStore {
+  todos = [];
+  pendingRequests = 0;
 
   constructor() {
-    makeAutoObservable(this);
+    makeObservable(this, {
+      todos: observable,
+      pendingRequests: observable,
+      completedTodosCount: computed,
+      report: computed,
+      addTodo: action,
+    });
+    autorun(() => console.log(this.report));
   }
 
-  increase() {
-    this.secondsPassed += 1;
+  get completedTodosCount() {
+    return this.todos.filter(
+      todo => todo.completed === true
+    ).length;
   }
 
-  reset() {
-    this.secondsPassed = 0;
+  get report() {
+    if (this.todos.length === 0)
+      return "<none>";
+    const nextTodo = this.todos.find(todo => todo.completed === false);
+    return `Next todo: "${nextTodo ? nextTodo.task : "<none>"}". ` +
+      `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+  }
+
+  addTodo(task) {
+    this.todos.push({
+      task: task,
+      completed: false,
+      assignee: null
+    });
   }
 }
 
-const myTimer = new Timer();
+const observableTodoStore = new ObservableTodoStore();
+                        
+const TodoList = observer(({store}) => {
+  const onNewTodo = () => {
+    store.addTodo(prompt('Enter a new todo:','coffee plz'));
+  }
 
-setInterval(() => {
-  myTimer.increase()
-}, 1000);
+  return (
+    <div>
+      { store.report }
+      <ul>
+        { store.todos.map(
+          (todo, idx) => <TodoView todo={ todo } key={ idx } />
+        ) }
+      </ul>
+      { store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null }
+      <button onClick={ onNewTodo }>New Todo</button>
+      <small> (double-click a todo to edit)</small>
+    </div>
+  );
+})
 
+const TodoView = observer(({todo}) => {
+  const onToggleCompleted = () => {
+    todo.completed = !todo.completed;
+  }
+
+  const onRename = () => {
+    todo.task = prompt('Task name', todo.task) || todo.task;
+  }
+
+  return (
+    <li onDoubleClick={ onRename }>
+      <input
+        type='checkbox'
+        checked={ todo.completed }
+        onChange={ onToggleCompleted }
+      />
+      { todo.task }
+      { todo.assignee
+        ? <small>{ todo.assignee.name }</small>
+        : null
+      }
+    </li>
+  );
+})
 function App() {
   return (
     <div>
-      <TimerView timer={myTimer}/>
+      <TodoList store={observableTodoStore}></TodoList>
     </div>
   );
 }
