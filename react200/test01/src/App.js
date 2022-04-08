e76d09482fd708a53ea1df18ad5958483762f683
jiyoung <shyunji023@gmail.com>
2022-04-08 15:07:31 +0900

    ObservableTodoStore

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,57 +1,79 @@
-import { autorun, computed, configure, makeObservable, observable } from 'mobx';
+import { action, autorun, computed, configure, makeAutoObservable, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 
-configure({enforceActions:'never',})
-class OrderLine {
-  price = 0
-  amount = 1
+class ObservableTodoStore {
+  todos = [];
 
   constructor() {
     makeObservable(this, {
-      price: observable,
-      amount: observable,
-      total: computed
-    })
-    autorun(() => {
-      console.log("autorun:1 " + this.total)
-    })
+      todos: observable,
+      completedTodosCount: computed,
+      report: computed,
+      addTodo: action,
+    });
+    autorun(() => console.log(this.report));
   }
 
-  get total() {
-    console.log("Computing...")
-    return this.price * this.amount
+  get completedTodosCount() {
+    console.log('completedTodosCount');
+    return this.todos.filter(
+      todo => todo.completed === true
+    ).length;
+  }
+
+  get report() {
+    console.log('report');
+    if (this.todos.length === 0)
+      return "<none>";
+    const nextTodo = this.todos.find(todo => todo.completed === false);
+    // return 'tiger';
+    return `Next todo: "${nextTodo ? nextTodo.task : "<none>"}". ` +
+      `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+  }
+
+  addTodo(task) {
+    console.log('addTodo');
+    this.todos.push({ task: task, completed: false,});
   }
 }
 
-const order = new OrderLine(0);
+const observableTodoStore = new ObservableTodoStore();
 console.log('------------------------------');
+
 class App extends Component {
   f1 = ()=>{
-    console.log(order.total);
+    observableTodoStore.addTodo("read MobX tutorial");
   }
   f2 = ()=>{
-    order.price+=1;
+    observableTodoStore.addTodo("try MobX");
   }
   f3 = ()=>{
-    order.amount+=10;
+    observableTodoStore.todos[0].completed = true;
   }
   f4 = ()=>{
-    order.amount = 0;
-    order.price = 0;
+    observableTodoStore.todos[1].task = "try MobX in own project";
+  }
+  f5 = ()=>{
+    observableTodoStore.todos[0].task = "grok MobX tutorial";
   }
   render() {
     return (
       <div>
-        <button onClick={this.f1}>total버튼</button><br />
-        <button onClick={this.f2}>price버튼</button><br />
-        <button onClick={this.f3}>amount버튼</button><br />
-        <button onClick={this.f4}>reset버튼</button><br />
-        <h1>{order.price}</h1>
-        <h1>{order.total}</h1>
-        <h1>{order.amount}</h1>
+        <button onClick={this.f1}>버튼</button>
+        <button onClick={this.f2}>버튼</button>
+        <button onClick={this.f3}>버튼</button>
+        <button onClick={this.f4}>버튼</button>
+        <button onClick={this.f5}>버튼</button>
+        <h1>{observableTodoStore.completedTodosCount}</h1>
+        {
+          observableTodoStore.todos.map((value, index)=>{
+            return <h1 key ={index}>{value.task}{value.completed.toString()}</h1>
+          })
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default observer(App);
