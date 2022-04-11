4baa44fa7b1c79f0babc816629e7ac27d67ff467
jiyoung <shyunji023@gmail.com>
2022-04-11 11:34:59 +0900

    debounce

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -4,17 +4,27 @@ import Header from './Header admin';
 import Footer from './Footer';
 import './css/new.css';
 import LoginForm from './LoginForm';
+import { debounce } from 'lodash';
 class App extends Component {
+  f1 = debounce(()=>{
+    console.log('debounce');
+  },1000); 
+  
+  f2 = debounce(()=>{
+    console.log('debounce');
+  },1000); 
   
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header />
         <Routes>
           <Route path="/" element={<LoginForm />}/>
         </Routes>
         <Footer />
-        <img src={require('./img/layout/logo.jpg')} alt="logo"></img>
+        <img src={require('./img/layout/logo.jpg')} alt="logo"></img> */}
+        <button onClick={this.f1}>버튼</button>
+        <input type='text' onChange={this.f2}/>
       </div>
     );
   }
