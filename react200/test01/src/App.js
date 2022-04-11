01a5fac5a881f2ae42e8e0a3d5e680c7e5aec9a6
jiyoung <shyunji023@gmail.com>
2022-04-11 11:26:34 +0900

    loginForm 예제

--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
-import cookie from 'react-cookies';
-
+import { Link, Route, Routes } from 'react-router-dom';
+import Header from './Header admin';
+import Footer from './Footer';
+import './css/new.css';
+import LoginForm from './LoginForm';
 class App extends Component {
-  f1 = () =>{
-    cookie.save('tiger',1000);
-    cookie.save('lion',2000);
-    cookie.save('dog',3000);
-  }
-  f2 = () =>{
-    const value = cookie.load('lion');
-    alert(value);
-  }
-  f3 = () =>{
-    cookie.remove('tiger');
-  }
-  f4 = () =>{
-    cookie.save('tiger',4000);
-    cookie.save('lion',5000);
-    cookie.save('dog',6000);
-  }
+  
   render() {
     return (
       <div>
-        <button onClick={this.f1}>쿠키 추가</button>
-        <button onClick={this.f2}>쿠키 검색</button>
-        <button onClick={this.f3}>쿠키 제거</button>
-        <button onClick={this.f4}>쿠키 수정</button>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LoginForm />}/>
+        </Routes>
+        <Footer />
+        <img src={require('./img/layout/logo.jpg')} alt="logo"></img>
       </div>
     );
   }
--- react200/test01/src/index.js
+++ react200/test01/src/index.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   //<React.StrictMode>
-    <App />,
+  // App 컴포넌트는 라우팅 설정 전용이다
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   //</React.StrictMode>,
   document.getElementById('root')
 );
