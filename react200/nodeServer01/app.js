eb9d7d2619e6b1f237c3fc435bad3505b6d765f0
jiyoung <shyunji023@gmail.com>
2022-04-14 09:41:18 +0900

    mybatis 기초

--- react200/nodeServer01/app.js
+++ react200/nodeServer01/app.js
@@ -8,14 +8,22 @@ app.use(express.urlencoded({ extended: false }));
 
 var router = express.Router();
 //------------------------------------------
+// mybatis
 app.use('/api/Swtool',
   router.post('/',function(req, res, next){
     console.log(1);
-    router.post('/',function(req, res, next){
-      console.log(2);
-    }),
+    req.body.mapper = 'SwToolsMapper';
+    req.body.crud = 'select';
+    req.body.mapper_id = 'selectSwToolList';
     next('route');
   }),
+  router.post('/',function(req, res, next){
+    let param = req.body;
+    console.log(param);
+    // p.363 22라인 해석
+    console.log(param.mapper+'.xml');
+    console.log(2);
+  })
 );
 
 //------------------------------------------
