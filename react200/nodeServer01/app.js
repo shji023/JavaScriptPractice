54a815fa560c566ef5112b053e777ce63d91168d
jiyoung <shyunji023@gmail.com>
2022-04-18 12:15:09 +0900

    db자동 insert

--- react200/nodeServer01/app.js
+++ react200/nodeServer01/app.js
@@ -1,6 +1,7 @@
 // express 모듈을 불러온다.
 const { request } = require('express');
 var express = require('express');
+require("./routes/BatchRout");
 // app변수로 express를 호출한다.
 var app = express();
 
@@ -11,53 +12,63 @@ var router = express.Router();
 
 //------------------------------------------
 // mybatis
-app.use('/api/Swtool',
-  router.post('/',function(req, res, next){
-    console.log(1);
-    if(req.query.type == 'list'){
-      req.body.mapper = 'SwToolsMapper';
-      req.body.crud = 'select';
-      req.body.mapper_id = 'selectSwToolsList';
+// app.use('/api/Swtool',
+//   router.post('/',function(req, res, next){
+//     console.log(1);
+//     if(req.query.type == 'list'){
+//       req.body.mapper = 'SwToolsMapper';
+//       req.body.crud = 'select';
+//       req.body.mapper_id = 'selectSwToolsList';
 
-      router.use('/',require('./dbconnect_Module'));
-      next('route');
-    }else if(req.query.type == 'save'){
+//       router.use('/',require('./dbconnect_Module'));
+//       next('route');
+//     }else if(req.query.type == 'save'){
 
-      req.body.mapper = 'SwToolsMapper';//mybatis xml 파일명
-      req.body.crud = 'insert';//select, insert, update, delete 중에 입력
-      req.body.mapper_id = 'insertSwToolsInfo';
+//       req.body.mapper = 'SwToolsMapper';//mybatis xml 파일명
+//       req.body.crud = 'insert';//select, insert, update, delete 중에 입력
+//       req.body.mapper_id = 'insertSwToolsInfo';
 
-      router.use('/', require('./dbconnect_Module'));
-      next('route')
+//       router.use('/', require('./dbconnect_Module'));
+//       next('route')
 
-    }else if(req.query.type == 'modify'){
-      try {
-        req.body.mapper = 'SwToolsMapper';
-        req.body.crud = 'update';//select, insert, update, delete 중에 입력
-        req.body.mapper_id = 'updateSwToolsInfo';
+//     }else if(req.query.type == 'modify'){
+//       try {
+//         req.body.mapper = 'SwToolsMapper';
+//         req.body.crud = 'update';//select, insert, update, delete 중에 입력
+//         req.body.mapper_id = 'updateSwToolsInfo';
         
-        router.use('/', require('./dbconnect_Module'));
-        next('route')
-      } catch (error) {
-        console.log("Module > dbconnect error : "+ error);      
-      }
-    }else if(req.query.type === 'delete'){
-      //Swtool 삭제
-      try {
-        //Mysql 쿼리 호출정보 입력
-        req.body.mapper = 'SwToolsMapper';//mybatis xml 파일명
-        req.body.crud = 'delete';//select, insert, update, delete 중에 입력
-        req.body.mapper_id = 'deleteSwToolsInfo';
+//         router.use('/', require('./dbconnect_Module'));
+//         next('route')
+//       } catch (error) {
+//         console.log("Module > dbconnect error : "+ error);      
+//       }
+//     }else if(req.query.type === 'delete'){
+//       //Swtool 삭제
+//       try {
+//         //Mysql 쿼리 호출정보 입력
+//         req.body.mapper = 'SwToolsMapper';//mybatis xml 파일명
+//         req.body.crud = 'delete';//select, insert, update, delete 중에 입력
+//         req.body.mapper_id = 'deleteSwToolsInfo';
         
-        router.use('/',  require('./dbconnect_Module'));
-        next('route')
-      } catch (error) {
-        console.log("Module > dbconnect error : "+ error);      
-      }
-    }
-  }),
-);
+//         router.use('/',  require('./dbconnect_Module'));
+//         next('route')
+//       } catch (error) {
+//         console.log("Module > dbconnect error : "+ error);      
+//       }
+//     }
+//   }),
+// );
 
+// app.use('/save',
+//   router.post('/',function(req,res,next){
+//     console.log(2);
+//     console.log(req.body);
+//     req.body.mapper="SwToolSMapper"
+//     req.body.crud="insert"
+//     req.body.crud,id="insertOne"
+//     res.send('OK');
+//   }),
+//   )
 //------------------------------------------
 var port = process.env.PORT || '5000';
 app.listen(port, ()=>{console.log('listen');});
--- react200/test01/src/App.js
+++ react200/test01/src/App.js
@@ -1,110 +1,40 @@
-import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import axios from 'axios';
 import React, { Component } from 'react';
-import reactElementToJSXString from 'react-element-to-jsx-string';
 
 class App extends Component {
   constructor(props){
     super(props);
-
-    this.state={
-      a:undefined,
-      b:undefined
-    }
+    this.state={name:'몽키',salary:300}
   }
-  f1 = ()=>{
-    let res = [
-      {id:1, name:'지렁이',salary:100, bonus:10},
-      {id:2, name:'지렁이2',salary:200, bonus:5},
-    ];
-    this.setState({a:res},()=>{
+  handleChange=(e)=>{
+    this.setState({[e.target.name]:e.target.value},()=>{
       console.log(this.state);
-      console.log(this.state.a);
-      console.log(this.state.a[0].id); // 1
     });
   }
-  // 즉시갱신 이루어짐
-  f2 = ()=>{
-    let result = [];
-    for (let i = 0; i < 2; i++) {
-      result.push(
-        <tr key={i}>
-          <td>{i*3+1}</td>
-          <td>{i*3+2}</td>
-          <td>{i*3+3}</td>
-        </tr>
-      );
-      this.setState({b:result},()=>{
-        for (let i = 0; i < result.length; i++) {
-          console.log(reactElementToJSXString(this.state.b[i]));
-        }
-      });
+  handleSumbit=(e)=>{
+    const user = {
+      name:this.state.name, 
+      salary:this.state.salary,
     }
-  }
-  // 즉시갱신 이루어짐
-  f3 = ()=>{
-    let res = [
-      {id:1, name:'지렁이',salary:100, bonus:10},
-      {id:2, name:'지렁이2',salary:200, bonus:5},
-    ];
-    this.setState({a:res},()=>{
-      let result = [];
-      for (let i = 0; i < 2; i++) {
-        result.push(
-          <tr key={i}>
-            <td>{this.state.a[i].id}</td>
-            <td>{this.state.a[i].name}</td>
-            <td>{this.state.a[i].salary}</td>
-          </tr>
-        );
-      }
-      this.setState({b:result});
-    });
-  }
-  f4 = ()=>{
-    let res = [
-      {id:1, name:'지렁이',salary:100, bonus:10},
-      {id:2, name:'지렁이2',salary:200, bonus:5},
-    ];
-    this.setState({a:res},()=>{
-      this.setState({b:this.tableAppend()});
-    })
-  }
-  tableAppend = ()=>{
-    let result = [];
-    for (let i = 0; i < 2; i++) {
-      result.push(
-        <tr key={i}>
-          <td>{this.state.a[i].id}</td>
-          <td>{this.state.a[i].name}</td>
-          <td>{this.state.a[i].salary}</td>
-        </tr>
-      );
-    }
-    return result;
-  }
-  f5 = ()=>{
-    axios.post('/api/Swtool?type=list',{})
-    .then(res=>{
+    axios.post('/save',{user})
+    .then((res)=>{
       console.log(res.data);
-      this.setState({a:res.data});
-      this.setState({b:this.tableAppend()});
-    })
+    });
+    e.preventDefault();
   }
-
-  render() { 
+  render() {
     return (
       <div>
-        <button onClick={this.f1}>클릭1</button>
-        <button onClick={this.f2}>클릭2</button>
-        <button onClick={this.f3}>클릭3</button>
-        <button onClick={this.f4}>클릭4</button>
-        <button onClick={this.f5}>클릭5</button>
-        <table border='1'>
-          <tbody>
-            {this.state.b}
-          </tbody>
-        </table>
+        <form onSubmit={this.handleSumbit}>
+          <input type='text' name='name'
+          defaultValue={this.state.name}
+          onChange={this.handleChange} />
+          <input type='text' name='salary'
+          defaultValue={this.state.salary}
+          onChange={this.handleChange} />
+          <button type='submit'>추가</button>
+        </form>
+        
       </div>
     );
   }
